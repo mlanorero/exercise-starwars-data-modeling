bad5fc55bce1bd080a33d1fd331a7d7ab19ae7a9
mlanorero <mlanorero@gmail.com>
2021-11-25 16:12:49 +0000

    Hecho

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
+# uno a muchos, una persona puede tener hartas emails
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
@@ -15,16 +15,37 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Email_Address(Base):
+    __tablename__ = 'email_adres'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    email_user = Column(String(250))
+    simbol = Column(String(250))
+    email_domain = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+
+# uno a uno, una persona puede tener una cuenta en el blog
+
+class Username(Base):
+    __tablename__= 'username'
+    id = Column(Integer, primary_key=True)
+    email_user = Column(String(250))
+    username = Column(String(250))
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+# uno a mucho, un usuario o persona puede guardar muchos personajes favoritos
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    email_user = Column(String(250))
+    username = Column(String(250))
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Username)
+
 
     def to_dict(self):
         return {}
